3697f7d97ea6586b55ca75969521a01ea8fbc620
sidhardha <reddyvenkatasaisidhardha03@gmail.com>
2026-05-10 14:52:41 +0530

    Added JWT login authentication

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -6,11 +6,12 @@ const router = express.Router();
 
 const pool = require("../config/db");
 
+
+// REGISTER
 router.post("/register", async (req, res) => {
   try {
     const { name, email, password } = req.body;
 
-    // check existing user
     const existingUser = await pool.query(
       "SELECT * FROM users WHERE email = $1",
       [email]
@@ -22,7 +23,6 @@ router.post("/register", async (req, res) => {
       });
     }
 
-    // hash password
     const salt = await bcrypt.genSalt(10);
 
     const hashedPassword = await bcrypt.hash(
@@ -30,7 +30,6 @@ router.post("/register", async (req, res) => {
       salt
     );
 
-    // save user
     const newUser = await pool.query(
       `INSERT INTO users (name, email, password)
        VALUES ($1, $2, $3)
@@ -42,6 +41,67 @@ router.post("/register", async (req, res) => {
       message: "User registered successfully",
       user: newUser.rows[0],
     });
+
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      message: "Server error",
+    });
+  }
+});
+
+
+// LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const userResult = await pool.query(
+      "SELECT * FROM users WHERE email = $1",
+      [email]
+    );
+
+    if (userResult.rows.length === 0) {
+      return res.status(400).json({
+        message: "Invalid email or password",
+      });
+    }
+
+    const user = userResult.rows[0];
+
+    const isMatch = await bcrypt.compare(
+      password,
+      user.password
+    );
+
+    if (!isMatch) {
+      return res.status(400).json({
+        message: "Invalid email or password",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "7d",
+      }
+    );
+
+    res.status(200).json({
+      message: "Login successful",
+      token,
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+
   } catch (error) {
     console.log(error);
 
