bc3f34dde9ac9c8a7ce7dfbbc480dd4dcb8762f6
sidhardha <reddyvenkatasaisidhardha03@gmail.com>
2026-05-16 11:58:05 +0530

    Added user order history API

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -1,9 +1,7 @@
 const express = require("express");
-
 const router = express.Router();
 
 const pool = require("../config/db");
-
 const authMiddleware = require("../middleware/authMiddleware");
 
 
@@ -26,17 +24,41 @@ router.post("/", authMiddleware, async (req, res) => {
 
     res.status(201).json({
       message: "Order placed successfully",
-      order: newOrder.rows[0],
+      order: newOrder.rows[0]
     });
 
   } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Server error"
+    });
+  }
+});
 
+
+// GET USER ORDERS
+router.get("/", authMiddleware, async (req, res) => {
+  try {
+
+    const userId = req.user.id;
+
+    const orders = await pool.query(
+      `
+      SELECT * FROM orders
+      WHERE user_id = $1
+      ORDER BY created_at DESC
+      `,
+      [userId]
+    );
+
+    res.status(200).json(orders.rows);
+
+  } catch (error) {
     console.log(error);
 
     res.status(500).json({
-      message: "Server error",
+      message: "Server error"
     });
-
   }
 });
 
